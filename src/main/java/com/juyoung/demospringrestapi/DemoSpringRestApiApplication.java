079eb792ff534a609278d11ad38f44db1cebe349
Juyoung <juy5790@outlook.com>
2019-04-02 22:47:30 +0900

    create project

---
+++ src/main/java/com/juyoung/demospringrestapi/DemoSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.juyoung.demospringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringRestApiApplication.class, args);
+    }
+
+}
