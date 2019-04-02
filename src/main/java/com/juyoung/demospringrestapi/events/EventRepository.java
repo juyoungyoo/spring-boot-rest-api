05ec43de2f49a3e466852ce6976618ee1c87f729
Juyoung <juy5790@outlook.com>
2019-04-03 00:50:28 +0900

    11. Event 생성 API 구현 : EventRepository 구현 [2]

---
+++ src/main/java/com/juyoung/demospringrestapi/events/EventRepository.java
@@ -0,0 +1,5 @@
+package com.juyoung.demospringrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> { }
