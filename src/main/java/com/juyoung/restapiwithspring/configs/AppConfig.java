940168573d4b331f196e7dc3ab143b309cea7beb
Juyoung <juy5790@outlook.com>
2019-10-02 00:49:22 +0900

    feat #setting modelMapper library 제거

--- src/main/java/com/juyoung/restapiwithspring/configs/AppConfig.java
+++ src/main/java/com/juyoung/restapiwithspring/configs/AppConfig.java
@@ -1,30 +1,14 @@
 package com.juyoung.restapiwithspring.configs;
 
-import com.juyoung.restapiwithspring.accounts.Account;
-import com.juyoung.restapiwithspring.accounts.AccountRepository;
-import com.juyoung.restapiwithspring.accounts.AccountService;
-import com.juyoung.restapiwithspring.accounts.RoleType;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Component
+@EnableJpaAuditing
 public class AppConfig {
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
